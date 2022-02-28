bc3398b4cd166fc96b070f61071b33d147966861
Carl Nysten <nysten.carl@gmail.com>
2022-02-28 22:33:52 +0200

    serialization.rs draft

---
+++ src/serialization.rs
@@ -0,0 +1,17 @@
+// This file corresponds to serialization.c of the c libagent
+
+pub fn serialize_agent_info(info: AgentInfo, buffer: String) {
+    unimplemented!("");
+}
+
+pub fn deserialize_agent_info(buffer: String) -> AgentInfo {
+    unimplemented!("");
+}
+
+pub fn serialize_agent_command(command: Command, buffer: String) {
+    unimplemented!("");
+}
+
+pub fn deserialize_agent_command(command: Command, buffer: String) {
+    unimplemented!("");
+}
