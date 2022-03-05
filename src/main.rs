d0b9179676b94e2a911e025f903c64c7e23f252d
Tuomas Louekari <tuoppil@gmail.com>
2022-03-05 15:39:24 +0200

    Rudimentary implementation of heatmap, bees swarm toward enemy hive

--- src/main.rs
+++ src/main.rs
@@ -3,75 +3,77 @@ mod common;
 mod serialization;
 mod think;
 mod utils;
+use array2d::Array2D;
 
 use std::env;
 use crate::common::{ 
-    Action,
-    AgentInfo,
-    Cell,
-    Command,
-    Coords,
-    Direction,
-    VIEW_DISTANCE
+	Action,
+	AgentInfo,
+	Cell,
+	Command,
+	Coords,
+	Direction,
+	VIEW_DISTANCE
 };
-use crate::think::{ find_neighbour };
+use crate::think::*;
 
 /// Returns the hive cell type given a player number.
 pub fn hive_cell(player: i32) -> Cell {
-    if player == 0 {
-        Cell::HIVE_0
-    } else {
-        Cell::HIVE_1
-    }
+	if player == 0 {
+		Cell::HIVE_0
+	} else {
+		Cell::HIVE_1
+	}
 }
 
 /// The function that decides on a command for a given turn based on agent info.
 /// Current logic is similar to the logic in example agent.
-pub fn think(info: &AgentInfo) -> Command {
-    let bee_cell = info.cell_type(&Coords { row: VIEW_DISTANCE, col: VIEW_DISTANCE });
+pub fn think(info: &AgentInfo, heatmap: &Array2D<f32>) -> Command {
+	let bee_cell = info.cell_type(&Coords { row: VIEW_DISTANCE, col: VIEW_DISTANCE });
 
-    // If the current bee holds a flower, check if the hive is adjacent
-    // and forage the flower to the hive if possible.
-    if bee_cell.has_flower() {
-        let hive_direction = find_neighbour(info, &hive_cell(info.player));
-        match hive_direction {
-            Some(v) => return Command {
-                action: Action::FORAGE,
-                direction: v,
-            },
-            None => (),
-        }
-    // If the current bee doesn't hold a flower, check if there is a flower
-    // adjacent to the bee. Pick up if possible.
-    } else {
-        let flower_direction = find_neighbour(info, &Cell::FLOWER);
-        match flower_direction {
-            Some(v) => return Command {
-                action: Action::FORAGE,
-                direction: v,
-            },
-            None => (),
-        }
-    }
+	// If the current bee holds a flower, check if the hive is adjacent
+	// and forage the flower to the hive if possible.
+	if bee_cell.has_flower() {
+		let hive_direction = find_neighbour(info, &hive_cell(info.player));
+		match hive_direction {
+			Some(v) => return Command {
+				action: Action::FORAGE,
+				direction: v,
+			},
+			None => (),
+		}
+	// If the current bee doesn't hold a flower, check if there is a flower
+	// adjacent to the bee. Pick up if possible.
+	} else {
+		let wall_direction = find_heat(info, &heatmap);
+		//let flower_direction = find_neighbour(info, &Cell::FLOWER);
+		match wall_direction {
+			Some(v) => return Command {
+				action: Action::MOVE,
+				direction: v,
+			},
+			None => (),
+		}
+	}
 
-    // Otherwise move in a random direction.
-    let random_direction: Direction = rand::random();
-    Command {
-        action: Action::MOVE,
-        direction: random_direction,
-    }
+	// Otherwise move in a random direction.
+	let random_direction: Direction = rand::random();
+	Command {
+		action: Action::MOVE,
+		direction: random_direction,
+	}
 }
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+	let args: Vec<String> = env::args().collect();
 
-    if args.len() != 3 {
-        panic!("Usage: ./agent arena_host arena_ip");
-    }
+	if args.len() != 3 {
+		panic!("Usage: ./agent arena_host arena_ip");
+	}
 
-    let host: &String = args.get(1).unwrap();
-    let port: &String = args.get(2).unwrap();
-    let team_name = "builder-bees\n".to_string();
+	let host: &String = args.get(1).unwrap();
+	let port: &String = args.get(2).unwrap();
+	let team_name = "builder-bees\n".to_string();
 
-    agent::agent_main(host, port, &team_name, think).expect("Program should didn't exit in agent_main");
+	agent::agent_main(host, port, &team_name, think).expect("Program should didn't exit in agent_main");
 }
--- src/think.rs
+++ src/think.rs
@@ -1,22 +1,49 @@
 // This file contains functions used by the agent to choose an appropriate action.
 
 use crate::common::{ AgentInfo, Cell, Coords, Direction, VIEW_DISTANCE };
+use array2d::Array2D;
 use enum_iterator::IntoEnumIterator;
 
 /// Checks if surrounding cells have a cell of cell_type.
 /// If found, returns the direction of that cell wrapped in Option.
 pub fn find_neighbour(info: &AgentInfo, cell_type: &Cell) -> Option<Direction> {
-    let coords = Coords {
-        row: VIEW_DISTANCE,
-        col: VIEW_DISTANCE,
-    };
-    for direction in Direction::into_enum_iter() {
-        let adjacent = coords.adjacent_coord(&direction);
-        match adjacent {
-            Some(v) if info.cell_type(&v).eq(cell_type) => return Some(direction),
-            Some(_) => continue, //TODO: Figure out if this is ok
-            None => continue,
-        }
-    }
-    None
+	let coords = Coords {
+		row: VIEW_DISTANCE,
+		col: VIEW_DISTANCE,
+	};
+	for direction in Direction::into_enum_iter() {
+		let adjacent = coords.adjacent_coord(&direction);
+		match adjacent {
+			Some(v) if info.cell_type(&v).eq(cell_type) => return Some(direction),
+			Some(_) => continue, //TODO: Figure out if this is ok
+			None => continue,
+		}
+	}
+	None
+}
+
+pub fn find_heat(info: &AgentInfo, heatmap: &Array2D<f32>) -> Option<Direction> {
+	let coords = Coords {
+		row: info.row as usize,
+		col: info.col as usize,
+	};
+	let mut min_heat = 100.0;
+	let mut max_direction = Direction::N;
+	for direction in Direction::into_enum_iter() {
+		let adjacent = coords.adjacent_coord(&direction);
+		match adjacent {
+			Some(v) => {
+				let y = v.row;
+				let x = v.col;
+				let heat = heatmap.get(y, x).unwrap_or(&100.0);
+				if heat < &min_heat {
+					min_heat = *heat;
+					max_direction = direction;
+				}
+				println!("{:?} | {:?}", max_direction, heat);
+			},
+			None => continue,
+		}
+	}
+	Some(max_direction)
 }
